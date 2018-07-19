7a217be93547f626589c721a7ecfd187a7526acc
Hartwig Thomas <hartwig.thomas@enterag.ch>
2018-07-19 16:18:40 +0200

    2.1.66

--- src/ch/admin/bar/siard2/cmd/PrimaryDataTransfer.java
+++ src/ch/admin/bar/siard2/cmd/PrimaryDataTransfer.java
@@ -50,8 +50,12 @@ public class PrimaryDataTransfer
     _il.enter(mt.getName());
     /* schema mapping is null on download */
     TableMapping tm = null;
+    int iConcurrency = ResultSet.CONCUR_READ_ONLY;
     if (sm != null)
+    {
       tm = sm.getTableMapping(mt.getName());
+      iConcurrency = ResultSet.CONCUR_UPDATABLE;
+    }
     /* create query */
     StringBuilder sbSql = new StringBuilder("SELECT\r\n");
     List<List<String>> llColumnNames = mt.getColumnNames(supportsArrays(),supportsUdts());
@@ -83,7 +87,7 @@ public class PrimaryDataTransfer
     sbSql.append("\r\n FROM "+qiTable.format());
     /* execute query */
     _il.event(sbSql.toString());
-    Statement stmt = _conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+    Statement stmt = _conn.createStatement(ResultSet.TYPE_FORWARD_ONLY,iConcurrency);
     stmt.setQueryTimeout(_iQueryTimeoutSeconds);
     ResultSet rs = stmt.executeQuery(sbSql.toString());
     _il.exit(rs);
