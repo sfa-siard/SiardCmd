e6785a7ab317937df80161b432c4257f3f2b27a0
Hartwig Thomas <hartwig.thomas@enterag.ch>
2018-10-26 11:32:01 +0200

    2.1.104

--- src/ch/admin/bar/siard2/cmd/MetaDataFromDb.java
+++ src/ch/admin/bar/siard2/cmd/MetaDataFromDb.java
@@ -35,6 +35,7 @@ public class MetaDataFromDb
   /** logger */  
   private static IndentLogger _il = IndentLogger.getIndentLogger(MetaDataFromDb.class.getName());
   static final Pattern _patARRAY_CONSTRUCTOR = Pattern.compile("^\\s*(.*?)\\s+ARRAY\\s*\\[\\s*(\\d+)\\s*\\]$");
+  private boolean _bMaxLobNeeded = false;
   private MetaColumn _mcMaxLob = null;
   public MetaColumn getMaxLobColumn() { return _mcMaxLob; }
   private long _lMaxLobSize = -1;
@@ -1007,24 +1008,27 @@ public class MetaDataFromDb
     /* query rows and LOB sizes */
     int iLobs = 0;
     String sQuery = "SELECT COUNT(*) AS RECORDS";
-    for (int iColumn = 0; iColumn < mt.getMetaColumns(); iColumn++)
+    if (_bMaxLobNeeded)
     {
-      MetaColumn mc = mt.getMetaColumn(iColumn);
-      int iPreType = mc.getPreType();
-      if ((mc.getCardinality() < 0)
-          && ((iPreType == Types.CLOB) 
-          || (iPreType == Types.NCLOB)
-          || (iPreType == Types.BLOB)
-//        ||  (iPreType == Types.SQLXML) || // DB/2 stores XML as a hierarchical object tree ... 
-          ))
-      {
-        if (!mc.getTypeOriginal().equals("\"LONG\"")) // Oracle idiocy
-        {
-          sQuery = sQuery + ",\r\n SUM(OCTET_LENGTH("+SqlLiterals.formatId(mc.getName())+"))" +
-            " AS " + SqlLiterals.formatId(mc.getName()+"_SIZE");
-          iLobs++;
-        }
-      }
+	    for (int iColumn = 0; iColumn < mt.getMetaColumns(); iColumn++)
+	    {
+	      MetaColumn mc = mt.getMetaColumn(iColumn);
+	      int iPreType = mc.getPreType();
+	      if ((mc.getCardinality() < 0)
+	          && ((iPreType == Types.CLOB) 
+	          || (iPreType == Types.NCLOB)
+	          || (iPreType == Types.BLOB)
+	//        ||  (iPreType == Types.SQLXML) || // DB/2 stores XML as a hierarchical object tree ... 
+	          ))
+	      {
+	        if (!mc.getTypeOriginal().equals("\"LONG\"")) // Oracle idiocy
+	        {
+	          sQuery = sQuery + ",\r\n SUM(OCTET_LENGTH("+SqlLiterals.formatId(mc.getName())+"))" +
+	            " AS " + SqlLiterals.formatId(mc.getName()+"_SIZE");
+	          iLobs++;
+	        }
+	      }
+	    }
     }
     QualifiedId qiTable = new QualifiedId(null,mt.getParentMetaSchema().getName(),mt.getName());
     sQuery = sQuery + "\r\nFROM "+qiTable.format();
@@ -1036,19 +1040,22 @@ public class MetaDataFromDb
     {
       long lRows = rsSizes.getLong("RECORDS");
       mt.setRows(lRows);
-      for (int iLob = 0; iLob < iLobs; iLob++)
+      if (_bMaxLobNeeded)
       {
-        String sLobName = rsmd.getColumnLabel(iLob+2);
-        long lLobSize = rsSizes.getLong(sLobName);
-        if (_lMaxLobSize < lLobSize)
-        {
-          _lMaxLobSize = lLobSize;
-          _mcMaxLob = mt.getMetaColumn(sLobName.substring(0,sLobName.length()-"_SIZE".length())); 
-        }
+	      for (int iLob = 0; iLob < iLobs; iLob++)
+	      {
+	        String sLobName = rsmd.getColumnLabel(iLob+2);
+	        long lLobSize = rsSizes.getLong(sLobName);
+	        if (_lMaxLobSize < lLobSize)
+	        {
+	          _lMaxLobSize = lLobSize;
+	          _mcMaxLob = mt.getMetaColumn(sLobName.substring(0,sLobName.length()-"_SIZE".length())); 
+	        }
+	      }
       }
     }
     else
-      throw new IOException("Sizes of table "+mt.getName()+" could not be determined!");
+      throw new IOException("Size of table "+mt.getName()+" could not be determined!");
     rsSizes.close();
     stmtSizes.close();
   } /* getRows */
@@ -1347,13 +1354,14 @@ public class MetaDataFromDb
    * @throws IOException if an I/O error occurred.
    * @throws SQLException if a database error occurred.
    */
-  public void download(boolean bViewsAsTables, Progress progress)
+  public void download(boolean bViewsAsTables, boolean bMaxLobNeeded, Progress progress)
     throws IOException, SQLException
   {
     _il.enter();
     System.out.println("Meta Data");
     _progress = progress;
     _bViewsAsTables = bViewsAsTables;
+    _bMaxLobNeeded = bMaxLobNeeded;
     /* global meta data */
     logDownload();
     /* get tables (and Types and relevant schemas) */
