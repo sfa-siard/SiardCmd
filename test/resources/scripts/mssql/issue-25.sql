b216a2ce33f181cc5cd1bbacb4fa35f2260e52a8
Max Burri <burri@puzzle.ch>
2023-08-25 14:01:33 +0200

    refactor: cleanup initialization script

--- test/resources/scripts/mssql/issue-25.sql
+++ test/resources/scripts/mssql/issue-25.sql
@@ -1,16 +1,8 @@
-USE [master]
-
-
-SET ANSI_NULLS ON
-
-SET ANSI_PADDING ON
-
 CREATE TYPE [dbo].[dtDay2] FROM [tinyint] NOT NULL;
 
-CREATE TABLE [dbo].[CollectionEvent]
+CREATE TABLE [dbo].[testtable]
 (
     [ID] [int] IDENTITY (1,1) NOT NULL,
-    [Day]     [dbo].[dtDay2]       NULL
+    [DAY]     [dbo].[dtDay2]       NULL
 );
 
-SET ANSI_PADDING OFF
