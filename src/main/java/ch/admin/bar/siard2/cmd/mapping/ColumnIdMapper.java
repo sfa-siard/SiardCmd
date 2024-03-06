1077a44f6691c5c5cbcc14fa71c685c465469514
Simon Bühlmann <sdbuehlmann@gmail.com>
2024-03-06 13:10:31 +0100

    Revert changes for fixing isssue with missing on-delete / on-update actions

--- src/main/java/ch/admin/bar/siard2/cmd/mapping/ColumnIdMapper.java
+++
@@ -1,7 +0,0 @@
-package ch.admin.bar.siard2.cmd.mapping;
-
-import ch.admin.bar.siard2.cmd.model.QualifiedColumnId;
-
-public interface ColumnIdMapper {
-    QualifiedColumnId map(QualifiedColumnId origQualifiedColumnId);
-}
--- src/main/java/ch/admin/bar/siard2/cmd/mapping/TableIdMapper.java
+++
@@ -1,7 +0,0 @@
-package ch.admin.bar.siard2.cmd.mapping;
-
-import ch.admin.bar.siard2.cmd.model.QualifiedTableId;
-
-public interface TableIdMapper {
-    QualifiedTableId map(QualifiedTableId origQualifiedTableId);
-}
