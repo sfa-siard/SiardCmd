296b850bad0b3acac7c6b0d17a2ab78f46b7e6f5
llorentelemmc <llorente@puzzle.ch>
2025-04-09 14:19:16 +0200

    Create integration test for jdbcbase issue 7

--- src/integrationTest/java/ch/admin/bar/siard2/cmd/utils/SqlScripts.java
+++ src/integrationTest/java/ch/admin/bar/siard2/cmd/utils/SqlScripts.java
@@ -3,6 +3,7 @@ package ch.admin.bar.siard2.cmd.utils;
 public class SqlScripts {
     public static class Oracle {
         public final static String CREATE_USER_WITH_ALL_PRIVILEGES = "config/oracle/00_create_user.sql";
+        public final static String JDBCBASE_7 = "config/oracle/jdbcbase7.sql";
     }
 
     public static class Postgres {
