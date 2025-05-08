6544b25aaa63dbed3df3984e58b624759fbcd953
llorentelemmc <llorente@puzzle.ch>
2025-05-08 10:56:51 +0200

    Add varchar(8000) to sql script

--- src/integrationTest/resources/issues/siardsuite115/varchar-types.sql
+++ src/integrationTest/resources/issues/siardsuite115/varchar-types.sql
@@ -7,12 +7,13 @@ CREATE TABLE "TestSchema"."VarCharTest"
     "text1" VARCHAR,
     "text2" VARCHAR(1),
     "text3" VARCHAR(255),
-    "text4" VARCHAR(MAX)
+    "text4" VARCHAR(8000),
+    "text5" VARCHAR(MAX)
 );
 
-INSERT INTO "TestSchema"."VarCharTest" ("Id", "text1", "text2", "text3", "text4")
+INSERT INTO "TestSchema"."VarCharTest" ("Id", "text1", "text2", "text3", "text4", "text5")
 VALUES
-    (1, 'a', 'b', 'Regular text', 'This is a longer text that would be stored as VARCHAR(MAX)'),
-    (2, 'c', 'd', 'Another text', 'Another longer text example'),
-    (3, 'e', 'f', 'Third example', ''),  -- Empty VARCHAR(MAX)
-    (4, 'g', 'h', 'Fourth example', NULL);  -- NULL VARCHAR(MAX)
+    (1, 'a', 'b', 'Regular text', 'Another longer text example', 'This is a longer text that would be stored as VARCHAR(MAX)'),
+    (2, 'c', 'd', 'Another text', 'Another longer text example', 'This is a longer text that would be stored as VARCHAR(MAX)'),
+    (3, 'e', 'f', 'Third example', '', ''),  -- Empty VARCHAR(MAX)
+    (4, 'g', 'h', 'Fourth example', NULL, NULL);  -- NULL VARCHAR(MAX)
