c693634a08562429beb2e0bd9c601a628e600a2a
llorentelemmc <llorente@puzzle.ch>
2025-05-19 09:17:04 +0200

    Integration test with nation db and mysql 5.7.4

---
+++ src/integrationTest/java/ch/admin/bar/siard2/cmd/issues/jdbcmysql4/MySQLDownloadVersionSupportIT.java
@@ -0,0 +1,44 @@
+package ch.admin.bar.siard2.cmd.issues.jdbcmysql4;
+
+import ch.admin.bar.siard2.cmd.SiardFromDb;
+import ch.admin.bar.siard2.cmd.utils.siard.SiardArchivesHandler;
+import ch.admin.bar.siard2.cmd.utils.SqlScripts;
+import lombok.val;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class MySQLDownloadVersionSupportIT {
+
+    @Rule
+    public SiardArchivesHandler siardArchivesHandler = new SiardArchivesHandler();
+
+    @Rule
+    public MySQLContainer<?> downloadDb = new MySQLContainer<>(DockerImageName.parse("mysql:5.7.40"))
+            .withUsername("root")
+            .withPassword("public")
+            .withDatabaseName("public")
+            .withInitScript(SqlScripts.MySQL.JDBCMYSQL_4)
+            .withConfigurationOverride("config/mysql");
+
+
+    @Test
+    public void downloadDb_expectNoException() throws SQLException, IOException, ClassNotFoundException {
+        val createdArchive = siardArchivesHandler.prepareEmpty();
+
+        SiardFromDb siardFromDb = new SiardFromDb(new String[]{
+                "-o",
+                "-j:" + downloadDb.getJdbcUrl() + "?zeroDateTimeBehavior=convertToNull",
+                "-u:" + downloadDb.getUsername(),
+                "-p:" + downloadDb.getPassword(),
+                "-s:" + createdArchive.getPathToArchiveFile()
+        });
+
+        Assert.assertEquals(SiardFromDb.iRETURN_OK, siardFromDb.getReturn());
+    }
+}
