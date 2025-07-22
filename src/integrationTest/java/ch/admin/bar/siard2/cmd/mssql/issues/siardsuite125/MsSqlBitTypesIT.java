0db565319aa5a21a17439e87f1551067e50cfbd4
Berivan Bertagna <bbertagna@puzzle.ch>
2025-07-22 11:07:11 +0200

    fix typo

--- src/integrationTest/java/ch/admin/bar/siard2/cmd/mssql/issues/siardsuite125/MsSqlBitTypesIT.java
+++ src/integrationTest/java/ch/admin/bar/siard2/cmd/mssql/issues/siardsuite125/MsSqlBitTypesIT.java
@@ -1,4 +1,4 @@
-package ch.admin.bar.siard2.cmd.mssql.issues.siarduite125;
+package ch.admin.bar.siard2.cmd.mssql.issues.siardsuite125;
 
 import ch.admin.bar.siard2.cmd.SiardFromDb;
 import ch.admin.bar.siard2.cmd.SiardToDb;
