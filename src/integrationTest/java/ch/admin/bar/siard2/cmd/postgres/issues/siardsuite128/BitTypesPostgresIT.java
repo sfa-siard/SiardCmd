b61dfd432baa8b285a6563235ce9e955247316c9
Berivan Bertagna <bbertagna@puzzle.ch>
2025-07-22 11:02:20 +0200

    test: add integration test for postgres metadata-extraction for bit types (WIP)

---
+++ src/integrationTest/java/ch/admin/bar/siard2/cmd/postgres/issues/siardsuite128/BitTypesPostgresIT.java
@@ -0,0 +1,64 @@
+package ch.admin.bar.siard2.cmd.postgres.issues.siardsuite128;
+
+import ch.admin.bar.siard2.cmd.SiardFromDb;
+import ch.admin.bar.siard2.cmd.utils.SqlScripts;
+import ch.admin.bar.siard2.cmd.utils.siard.SiardArchivesHandler;
+import lombok.val;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class BitTypesPostgresIT {
+
+    @Rule
+    public SiardArchivesHandler siardArchivesHandler = new SiardArchivesHandler();
+
+    @Rule
+    public PostgreSQLContainer<?> db = new PostgreSQLContainer<>(DockerImageName.parse("postgres:13"))
+            .withInitScript(SqlScripts.Postgres.SIARDSUITE_128_BIT);
+
+    @Test
+    public void uploadAndDownload_expectNoExceptions() throws IOException, SQLException, ClassNotFoundException {
+        // given
+        val createdArchive = siardArchivesHandler.prepareEmpty();
+
+        // when
+        SiardFromDb dbtoSiard = new SiardFromDb(new String[]{
+                "-o",
+                "-j:" + db.getJdbcUrl(),
+                "-u:" + db.getUsername(),
+                "-p:" + db.getPassword(),
+                "-s:" + createdArchive.getPathToArchiveFile()
+        });
+
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File desktopDir = new File("/home/bbertagna/Desktop");
+        File destFile = new File(desktopDir, "bit_type_test_" + timestamp + ".siard");
+        Files.copy(createdArchive.getPathToArchiveFile().toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        System.out.println("Archive copied to: " + destFile.getAbsolutePath());
+
+
+//        val siardArchive = siardArchivesHandler.prepareResource("issues/siardgui29/created-bit-type.siard");
+//        SiardToDb siardToDb = new SiardToDb(new String[]{
+//                "-o",
+//                "-j:" + db.getJdbcUrl(),
+//                "-u:" + db.getUsername(),
+//                "-p:" + db.getPassword(),
+//                "-s:" + siardArchive.getPathToArchiveFile()
+//        });
+
+        // then
+        Assert.assertEquals(SiardFromDb.iRETURN_OK, dbtoSiard.getReturn());
+        //Assert.assertEquals(SiardToDb.iRETURN_OK, siardToDb.getReturn());
+    }
+}
