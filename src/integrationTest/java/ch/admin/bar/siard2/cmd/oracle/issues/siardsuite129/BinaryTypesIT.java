ed0af16770398f3342957ffeb68df701755f5227
llorentelemmc <llorente@puzzle.ch>
2025-07-31 14:04:31 +0200

    Add metadata assertions for binary types

--- src/integrationTest/java/ch/admin/bar/siard2/cmd/oracle/issues/siardsuite129/BinaryTypesIT.java
+++ src/integrationTest/java/ch/admin/bar/siard2/cmd/oracle/issues/siardsuite129/BinaryTypesIT.java
@@ -6,7 +6,10 @@ import ch.admin.bar.siard2.cmd.utils.ConsoleLogConsumer;
 import ch.admin.bar.siard2.cmd.utils.SqlScripts;
 import ch.admin.bar.siard2.cmd.utils.TestResourcesResolver;
 import ch.admin.bar.siard2.cmd.utils.siard.SiardArchivesHandler;
+import ch.admin.bar.siard2.cmd.utils.siard.model.utils.Id;
+import ch.admin.bar.siard2.cmd.utils.siard.model.utils.QualifiedColumnId;
 import lombok.val;
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -40,9 +43,8 @@ public class BinaryTypesIT {
 
 
 
-    //Assert that siard archive created by siardcmd does not fail with the same exception when uploaded to db
     @Test
-    public void uploadCreatedArchive_expectNoExceptions() throws SQLException, IOException, ClassNotFoundException {
+    public void downloadArchive_expectNoExceptions() throws SQLException, IOException, ClassNotFoundException {
         val createdArchive = siardArchivesHandler.prepareEmpty();
 
         SiardFromDb dbtoSiard = new SiardFromDb(new String[]{
@@ -55,7 +57,62 @@ public class BinaryTypesIT {
 
         Assert.assertEquals(SiardFromDb.iRETURN_OK, dbtoSiard.getReturn());
 
+        val metadataExplorer = createdArchive.exploreMetadata();
+
+        val columnId = metadataExplorer.findByColumnId(QualifiedColumnId.builder()
+                .schemaId(Id.of("IT_USER"))
+                .tableId(Id.of("ORACLE_BINARY_TEST"))
+                .columnId(Id.of("ID"))
+                .build());
+        Assertions.assertThat(columnId.getType()).contains(Id.of("FLOAT(38)"));
+        Assertions.assertThat(columnId.getTypeOriginal()).contains(Id.of("NUMBER"));
+
+        val columnBitEquivalent = metadataExplorer.findByColumnId(QualifiedColumnId.builder()
+                .schemaId(Id.of("IT_USER"))
+                .tableId(Id.of("ORACLE_BINARY_TEST"))
+                .columnId(Id.of("BIT_EQUIVALENT"))
+                .build());
+        Assertions.assertThat(columnBitEquivalent.getType()).contains(Id.of("SMALLINT"));
+        Assertions.assertThat(columnBitEquivalent.getTypeOriginal()).contains(Id.of("NUMBER(1,0)"));
+
+        val columnRawSmall = metadataExplorer.findByColumnId(QualifiedColumnId.builder()
+                .schemaId(Id.of("IT_USER"))
+                .tableId(Id.of("ORACLE_BINARY_TEST"))
+                .columnId(Id.of("RAW_SMALL"))
+                .build());
+        Assertions.assertThat(columnRawSmall.getType()).contains(Id.of("VARBINARY(1)"));
+        Assertions.assertThat(columnRawSmall.getTypeOriginal()).contains(Id.of("RAW(1)"));
+
+        val columnRawMedium = metadataExplorer.findByColumnId(QualifiedColumnId.builder()
+                .schemaId(Id.of("IT_USER"))
+                .tableId(Id.of("ORACLE_BINARY_TEST"))
+                .columnId(Id.of("RAW_MEDIUM"))
+                .build());
+        Assertions.assertThat(columnRawMedium.getType()).contains(Id.of("VARBINARY(8)"));
+        Assertions.assertThat(columnRawMedium.getTypeOriginal()).contains(Id.of("RAW(8)"));
+
+        val columnRawLarge = metadataExplorer.findByColumnId(QualifiedColumnId.builder()
+                .schemaId(Id.of("IT_USER"))
+                .tableId(Id.of("ORACLE_BINARY_TEST"))
+                .columnId(Id.of("RAW_LARGE"))
+                .build());
+        Assertions.assertThat(columnRawLarge.getType()).contains(Id.of("VARBINARY(2000)"));
+        Assertions.assertThat(columnRawLarge.getTypeOriginal()).contains(Id.of("RAW(2000)"));
+
+        val columnBlobData = metadataExplorer.findByColumnId(QualifiedColumnId.builder()
+                .schemaId(Id.of("IT_USER"))
+                .tableId(Id.of("ORACLE_BINARY_TEST"))
+                .columnId(Id.of("BLOB_DATA"))
+                .build());
+        Assertions.assertThat(columnBlobData.getType()).contains(Id.of("BLOB"));
+        Assertions.assertThat(columnBlobData.getTypeOriginal()).contains(Id.of("BLOB"));
+    }
+    
+    //Assert that siard archive created by siardcmd is uploaded back to db
+    @Test
+    public void uploadCreatedArchive_expectNoExceptions() throws SQLException, IOException {
         val siardArchive = siardArchivesHandler.prepareResource("oracle/issues/siardsuite129/oracle-created-binary-types.siard");
+        
         SiardToDb siardToDb = new SiardToDb(new String[]{
                 "-o",
                 "-j:" + emptyDb.getJdbcUrl(),
@@ -63,7 +120,7 @@ public class BinaryTypesIT {
                 "-p:" + "password",
                 "-s:" + siardArchive.getPathToArchiveFile()
         });
+        
         Assert.assertEquals(SiardToDb.iRETURN_OK, siardToDb.getReturn());
-        // TODO: Assert that metadata was extracted once typeOriginal is added to SiardArchivesHandler
     }
 }
