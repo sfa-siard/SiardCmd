afd3f156fcab076f8178453ca766396d8c5cef0c
Max Burri <burri@puzzle.ch>
2025-09-08 16:56:58 +0200

    chore: upgrade to jdbc-orace 2.2.8
    note: this is the fallback release that reverts back to ojdbc7.jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     implementation("ch.admin.bar:enterutilities:v2.2.5")
     implementation("ch.admin.bar:jdbc-base:v2.2.11")
     implementation("ch.admin.bar:JdbcPostgres:v2.2.4")
-    implementation("ch.admin.bar:jdbcoracle:v2.2.7")
+    implementation("ch.admin.bar:jdbcoracle:v2.2.8")
     implementation("ch.admin.bar:jdbcmssql:v2.2.5")
     implementation("ch.admin.bar:jdbc-mysql:v2.2.5")
     implementation("ch.admin.bar:jdbc-access:v2.2.4")
